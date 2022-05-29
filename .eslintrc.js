862632c58020d4c12347f841db2e920dd4eb3ddf
Genesis Bell <bellmon90@hotmail.com>
2022-05-29 13:57:59 -0700

    fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,46 @@
 module.exports = {
-    root: true,
+    env: {
+      browser: true,
+      es2021: true,
+      es6: true,
+      node: true,
+      jest: true,
+    },
     extends: [
-        '@react-native-community',
-        'airbnb-typescript',
-        'prettier',
-        'prettier/@typescript-eslint',
-        'prettier/react',
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
     ],
-    ignorePatterns: [
-        '**/node_modules/**, node_modules/*, /node_modules*, node_modules/',
-    ],
-};
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+      project: './tsconfig.json',
+    },
+    plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+    rules: {
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      semi: ['error', 'always'],
+      'no-empty-function': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      'react/display-name': 'off',
+      'react/prop-types': 'off',
+      'prettier/prettier': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  };
+  
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,5 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
-  tabWidth: 4,
-  semi: true
+  printWidth: 100,
 };
